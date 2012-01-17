cacf6d11643f376e5030f616a336434e8a47a8d4
Darshan-Josiah Barber <darshan@darshancomputing.com>
2012-01-16 21:58:52 -0800

    Automatic update.

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+cd `dirname $0`
+
 wget https://code.google.com/feeds/issues/p/battery-indicator/issues/full -O all_issues.xml &&
 sed -i -e 's/full<\/id><updated>[^<]*<\/updated>/full<\/id><updated>NOW<\/updated>/' all_issues.xml &&
+
 git commit -am "Automatic update." &&
 git push; git push github
